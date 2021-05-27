9dac37974ffd00d2f6fa175ff970770481828b30
Naveenv01 <naveenv012001@gmail.com>
2021-05-27 13:32:12 +0530

    working

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:auth/Login.dart';
+import 'package:auth/SignUp.dart';
+import 'package:auth/Start.dart';
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+   runApp(MyApp());
+   }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      theme: ThemeData(
+        primaryColor: Colors.orange
+      ),
+      debugShowCheckedModeBanner: false,
+      home: 
+    
+      HomePage(),
+
+      routes: <String,WidgetBuilder>{
+
+        "Login" : (BuildContext context)=>Login(),
+        "SignUp":(BuildContext context)=>SignUp(),
+        "start":(BuildContext context)=>Start(),
+      },
+      
+    );
+  }
+
+}
+
+
+
